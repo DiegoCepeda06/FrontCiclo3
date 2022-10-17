f2a335221105fd6d871c43e2553ac4d8f4d1fb74
DiegoCepeda06​ <diegoa.cema@gmail.com>
2022-10-17 14:49:36 -0500

    first commit

---
+++ js/funcionesReportes.js
@@ -0,0 +1,71 @@
+function getStatus() {
+    $.ajax({
+        url: "http://localhost:8080/api/Reservation/report-status",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            pintarStatus(respuesta);
+        }
+    });
+
+}
+
+function pintarStatus(respuesta) {
+    let myTable = "<table>";
+    myTable += "<tr>";
+    myTable += "<td>" + respuesta.completed + "</td>";
+    myTable += "<td>" + respuesta.cancelled + "</td>";
+    myTable += "</tr>";
+
+    myTable += "</table>";
+    $("#resultado1").html(myTable);
+
+}
+
+function getFechas() {
+    $.ajax({
+        url: "http://localhost:8080/api/Reservation/report-dates/{dateOne}/{dateTwo}",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            pintarStatus(respuesta);
+        }
+    });
+
+}
+
+function pintarFechas() {
+    for (i = 0; i < respuesta.length; i++) {
+        let myTable = "<table>";
+        myTable += "<tr>";
+        myTable += "<td>" + respuesta[i].dateOne + "</td>";
+        myTable += "<td>" + respuesta[i].dateTwo + "</td>";
+        myTable += "</tr>";
+
+        myTable += "</table>";
+        $("#resultado2").html(myTable);
+    }
+}
+
+function getClientes() {
+
+    $.ajax({
+        url: "http://localhost:8080/api/Reservation/report-clients",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            pintarStatus(respuesta);
+        }
+    });
+
+}
+
+function pintarClientes() {
+    let myTable = "<table>";
+    myTable += "<tr>";
+    myTable += "<td>" + respuesta[i].idClient + "</td>";
+    myTable += "</tr>";
+
+    myTable += "</table>";
+    $("#resultado3").html(myTable);
+}
